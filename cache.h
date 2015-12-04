a2d8b3fdd391578ca433b8fa49e221ef89e5c5b4
rana <9205011@naver.com>
2015-12-03 22:31:38 -0800

    define MEMACC HDDACC

--- cache.h
+++ cache.h
@@ -1,11 +1,18 @@
 #ifndef __MAL__CACHE__
 #define __MAL__CACHE__
 
+#define MEMACC 100
+#define HDDACC 10000000 
+
+
 typedef struct mal_mem{
 	int page_num;
 	int dirty;
 	int used;
 }mal_mem;
+
+typedef mal_mem* mal_memp;
+
 
 int mem_access(int no, char mode);
 int stor_access(int no);
